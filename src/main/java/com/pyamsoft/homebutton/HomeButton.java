ed7f2c45e45f17aa1283034f9dc49e0aa9ba95b3
pyamsoft <pyam.soft@gmail.com>
2016-06-28 14:03:29 -0700

    Update to latest pydroid

--- src/main/java/com/pyamsoft/homebutton/HomeButton.java
+++ src/main/java/com/pyamsoft/homebutton/HomeButton.java
@@ -25,7 +25,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.NotificationCompat;
 import android.widget.Toast;
-import com.pyamsoft.pydroid.base.ApplicationBase;
+import com.pyamsoft.pydroid.base.app.ApplicationBase;
 
 public final class HomeButton extends ApplicationBase {
 
