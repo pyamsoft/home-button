29675a1c9332a47a479313467d71ca0b449a586f
pyamsoft <pyam.soft@gmail.com>
2016-09-05 10:14:58 -0700

    Update to pydroid 3.7.0

--- src/main/java/com/pyamsoft/homebutton/HomeButton.java
+++ src/main/java/com/pyamsoft/homebutton/HomeButton.java
@@ -16,53 +16,8 @@
 
 package com.pyamsoft.homebutton;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.NotificationCompat;
-import android.widget.Toast;
 import com.pyamsoft.pydroid.base.ApplicationBase;
-import com.squareup.leakcanary.LeakCanary;
 
 public class HomeButton extends ApplicationBase {
 
-  final static int ID = 1001;
-  final static int RC = 1004;
-
-  // The application is simple, so we don't really add options to enable or disable a notification
-  static void startHomeNotification(final Context c) {
-    final Intent home = new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_HOME);
-    final Context context = c.getApplicationContext();
-    final PendingIntent pe =
-        PendingIntent.getActivity(context, RC, home, PendingIntent.FLAG_UPDATE_CURRENT);
-    final Notification n = new NotificationCompat.Builder(context).setContentIntent(pe)
-        .setSmallIcon(R.mipmap.ic_launcher)
-        .setOngoing(true)
-        .setWhen(0)
-        .setNumber(0)
-        .setPriority(NotificationCompat.PRIORITY_MIN)
-        .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-        .setColor(ContextCompat.getColor(context, R.color.primary))
-        .setContentTitle(context.getResources().getString(R.string.app_name))
-        .setContentText(context.getString(R.string.press_to_home))
-        .build();
-    final NotificationManager notificationManager =
-        (NotificationManager) context.getApplicationContext()
-            .getSystemService(Context.NOTIFICATION_SERVICE);
-    notificationManager.notify(ID, n);
-  }
-
-  @Override protected void installInDebugMode() {
-    super.installInDebugMode();
-    LeakCanary.install(this);
-  }
-
-  @Override protected void installInNonTestMode() {
-    super.installInNonTestMode();
-    startHomeNotification(this);
-    Toast.makeText(this, getString(R.string.home_button_started), Toast.LENGTH_SHORT).show();
-  }
 }
