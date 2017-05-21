499ab40bdfe034ca5f5827697ea6a561e125c607
pyamsoft <pyam.soft@gmail.com>
2017-05-20 22:25:32 -0700

    Update to pydroid 9.5.0
    Initialize in the application class, remove content provider
    Update theme with some preference compat fixes, currently weird
    on API 19, but oh well.

--- homebutton/src/main/java/com/pyamsoft/homebutton/HomeButton.java
+++ homebutton/src/main/java/com/pyamsoft/homebutton/HomeButton.java
@@ -17,17 +17,27 @@
 package com.pyamsoft.homebutton;
 
 import android.app.Application;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.NotificationCompat;
+import android.widget.Toast;
+import com.pyamsoft.pydroid.about.Licenses;
 import com.pyamsoft.pydroid.helper.Checker;
-import com.pyamsoft.pydroid.ui.PYDroidApplication;
+import com.pyamsoft.pydroid.ui.PYDroid;
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
 
-public class HomeButton extends PYDroidApplication {
+public class HomeButton extends Application {
 
+  @NonNull private final Intent home =
+      new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_HOME);
   @Nullable private RefWatcher refWatcher;
 
   @CheckResult @NonNull static RefWatcher getRefWatcher(@NonNull Fragment fragment) {
@@ -39,24 +49,51 @@ public class HomeButton extends PYDroidApplication {
     }
   }
 
-  @Override protected boolean exitBeforeInitialization() {
-    return LeakCanary.isInAnalyzerProcess(this);
-  }
-
-  @Override protected void onCreateInDebugMode() {
-    super.onCreateInDebugMode();
-    refWatcher = LeakCanary.install(this);
-  }
-
-  @Override protected void onCreateInReleaseMode() {
-    super.onCreateInReleaseMode();
-    refWatcher = RefWatcher.DISABLED;
-  }
-
   @NonNull @CheckResult private RefWatcher getWatcher() {
-    if (refWatcher == null) {
-      throw new IllegalStateException("RefWatcher is NULL");
+    return Checker.checkNonNull(refWatcher);
+  }
+
+  @Override public void onCreate() {
+    super.onCreate();
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      return;
     }
-    return refWatcher;
+    PYDroid.initialize(this, BuildConfig.DEBUG);
+    if (BuildConfig.DEBUG) {
+      refWatcher = LeakCanary.install(this);
+    } else {
+      refWatcher = RefWatcher.DISABLED;
+    }
+
+    Licenses.create("Firebase", "https://firebase.google.com", "licenses/firebase");
+    startHomeNotification();
+
+    Toast.makeText(getApplicationContext(), getString(R.string.home_button_started),
+        Toast.LENGTH_SHORT).show();
+  }
+
+  // The application is simple, so we don't really add options to enable or disable a notification
+  private void startHomeNotification() {
+    int ID = 1001;
+    int RC = 1004;
+
+    NotificationManagerCompat notificationManager =
+        NotificationManagerCompat.from(getApplicationContext());
+
+    PendingIntent pe = PendingIntent.getActivity(getApplicationContext(), RC, home,
+        PendingIntent.FLAG_UPDATE_CURRENT);
+    Notification n = new NotificationCompat.Builder(getApplicationContext()).setContentIntent(pe)
+        .setSmallIcon(R.mipmap.ic_launcher)
+        .setOngoing(true)
+        .setWhen(0)
+        .setNumber(0)
+        .setPriority(NotificationCompat.PRIORITY_MIN)
+        .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+        .setColor(ContextCompat.getColor(getApplicationContext(), R.color.primary))
+        .setContentTitle(getString(R.string.app_name))
+        .setContentText(getString(R.string.press_to_home))
+        .build();
+
+    notificationManager.notify(ID, n);
   }
 }
