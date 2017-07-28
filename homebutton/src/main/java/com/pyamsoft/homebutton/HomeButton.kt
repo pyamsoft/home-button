d04cac25a13195e3b620dd5b271afe53e68ee573
pyamsoft <pyam.soft@gmail.com>
2017-07-27 19:45:17 -0700

    Support Android O

--- homebutton/src/main/java/com/pyamsoft/homebutton/HomeButton.kt
+++ homebutton/src/main/java/com/pyamsoft/homebutton/HomeButton.kt
@@ -17,18 +17,26 @@
 package com.pyamsoft.homebutton
 
 import android.app.Application
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.Build.VERSION_CODES
 import android.support.annotation.CheckResult
+import android.support.annotation.RequiresApi
 import android.support.v4.app.Fragment
+import android.support.v4.app.NotificationCompat
 import android.support.v4.app.NotificationManagerCompat
 import android.support.v4.content.ContextCompat
-import android.support.v7.app.NotificationCompat
 import com.pyamsoft.pydroid.about.Licenses
 import com.pyamsoft.pydroid.ui.PYDroid
 import com.pyamsoft.pydroid.ui.helper.Toasty
 import com.squareup.leakcanary.LeakCanary
 import com.squareup.leakcanary.RefWatcher
+import timber.log.Timber
 
 class HomeButton : Application() {
 
@@ -61,15 +69,38 @@ class HomeButton : Application() {
     val ID = 1001
     val RC = 1004
 
+    val notificationChannelId: String = "power_manager_foreground"
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      setupNotificationChannel(notificationChannelId)
+    }
+
     val pe = PendingIntent.getActivity(applicationContext, RC, home,
         PendingIntent.FLAG_UPDATE_CURRENT)
-    val n = NotificationCompat.Builder(applicationContext).setContentIntent(pe).setSmallIcon(
+    val n = NotificationCompat.Builder(applicationContext, notificationChannelId).setContentIntent(
+        pe).setSmallIcon(
         R.mipmap.ic_launcher).setOngoing(true).setWhen(0).setNumber(0).setPriority(
         NotificationCompat.PRIORITY_MIN).setVisibility(
         NotificationCompat.VISIBILITY_PUBLIC).setColor(
         ContextCompat.getColor(applicationContext, R.color.primary)).setContentTitle(
         getString(R.string.app_name)).setContentText(getString(R.string.press_to_home)).build()
     NotificationManagerCompat.from(applicationContext).notify(ID, n)
+  }
+
+  @RequiresApi(VERSION_CODES.O) private fun setupNotificationChannel(
+      notificationChannelId: String) {
+    val name = "Manager Service"
+    val description = "Notification related to the Power Manager service"
+    val importance = NotificationManagerCompat.IMPORTANCE_MIN
+    val notificationChannel = NotificationChannel(notificationChannelId, name, importance)
+    notificationChannel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+    notificationChannel.description = description
+    notificationChannel.enableLights(false)
+    notificationChannel.enableVibration(false)
+
+    Timber.d("Create notification channel with id: %s", notificationChannelId)
+    val notificationManager: NotificationManager = applicationContext.getSystemService(
+        Context.NOTIFICATION_SERVICE) as NotificationManager
+    notificationManager.createNotificationChannel(notificationChannel)
   }
 
   companion object {
